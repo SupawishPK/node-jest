b55021ed60e476756a28ce94afee97cd939e5bd4
Supawish kaewjing <supawish.kj@gmail.com>
2021-12-10 17:01:33 +0700

    feat: handle integration for get todo by id

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,6 +4,8 @@ const newTodo = require("../mock-data/new-todo.json");
 
 const endpointUrl = "/todos/";
 
+let firstTodo;
+
 describe(endpointUrl, () => {
   test("GET " + endpointUrl, async () => {
     const response = await request(app).get(endpointUrl);
@@ -12,6 +14,21 @@ describe(endpointUrl, () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
+    firstTodo = response.body[0];
+  });
+
+  test("GET by Id " + endpointUrl, async () => {
+    const response = await request(app).get(endpointUrl + firstTodo._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
+  });
+
+  test("GET todo by Id donsn't exist " + endpointUrl + ":todoId", async () => {
+    const response = await request(app).get(
+      endpointUrl + "61b2f17442a3d41c8a87c333"
+    );
+    expect(response.statusCode).toBe(404);
   });
 
   it("POST " + endpointUrl, async () => {

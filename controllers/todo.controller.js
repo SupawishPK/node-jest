d360c76aa77d26b4ff0eb40498bafd21f01ff09c
Supawish Kaewjing <supawish.kae@ascendcorp.com>
2021-12-10 15:59:00 +0700

    feat: handle error for get todo by id

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,7 +3,7 @@ const TodoModel = require("../model/todo.model");
 exports.createTodo = async (req, res, next) => {
   try {
     const createTodo = await TodoModel.create(req.body);
-    res.status(200).json(createTodo)
+    res.status(200).json(createTodo);
   } catch (err) {
     next(err);
   }
@@ -11,14 +11,18 @@ exports.createTodo = async (req, res, next) => {
 
 exports.getTodos = async (req, res, next) => {
   try {
-    const allTodos = await TodoModel.find({})
-    res.status(200).json(allTodos)
+    const allTodos = await TodoModel.find({});
+    res.status(200).json(allTodos);
   } catch (err) {
     next(err);
   }
-}
+};
 
 exports.getTodoById = async (req, res, next) => {
-  const todoModel = await TodoModel.findById(req.params.todoId)
-  res.status(200).json(todoModel)
-}
+  try {
+    const todoModel = await TodoModel.findById(req.params.todoId);
+    res.status(200).json(todoModel);
+  } catch (err) {
+    next(err);
+  }
+};
